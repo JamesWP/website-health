b0e9cdca3a3471dae7481b86fcb3a5744a464c5c
James Peach <james_peach01@hotmail.co.uk>
2025-02-22 00:26:28 +0000

    added status

--- checker/models.py
+++ checker/models.py
@@ -1,26 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
 class Website(models.Model):
     url = models.URLField()
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
     added_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} - {self.url}"
-    
+
+
 class WebsiteStatusEvent(models.Model):
     class StatusChoices(models.TextChoices):
-        UP = 'UP', ('Up')
-        DOWN = 'DOWN', ('Down')
-        NONE = 'NONE', ('None')
+        UP = "UP", ("Up")
+        DOWN = "DOWN", ("Down")
+        NONE = "NONE", ("None")
 
     website = models.ForeignKey(Website, on_delete=models.CASCADE)
-    
-    old_status = models.CharField(max_length = 4, choices = StatusChoices.choices)
-    new_status = models.CharField(max_length = 4, choices = StatusChoices.choices)
-    
+
+    old_status = models.CharField(max_length=4, choices=StatusChoices.choices)
+    new_status = models.CharField(max_length=4, choices=StatusChoices.choices)
+
     event_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.website} changed from {self.old_status} to {self.new_status}"
+
+
+def create_initial_status_event(instance, created, raw, **kwargs):
+    if not created or raw:
+        return
+
+    initial_status_event = WebsiteStatusEvent.objects.create(
+        website=instance,
+        old_status=WebsiteStatusEvent.StatusChoices.NONE,
+        new_status=WebsiteStatusEvent.StatusChoices.NONE,
+    )
+
+    initial_status_event.save()
+
+
+models.signals.post_save.connect(
+    create_initial_status_event,
+    sender=Website,
+    dispatch_uid="create_initial_status_event",
+)
